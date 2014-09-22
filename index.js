283ae435a885b0a1560edbc0042d7ad9cbc49150
Sebastien Renault <sre@zenexity.com>
2014-09-23 00:17:14 +0200

    Fix package.json

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var fs        = require('fs');
 var path      = require('path');
 var requirejs = require('requirejs');
 
-//node r.js -o baseUrl=. paths.jquery=some/other/jquery name=main out=main-built.js
-
 module.exports = function (options) {
     return es.mapSync(function (file, cb) {
         var destPath = path.join(file.cwd, options.baseUrl, path.basename(file.path));
